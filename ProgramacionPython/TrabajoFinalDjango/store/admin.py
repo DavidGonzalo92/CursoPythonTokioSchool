73c5806dd4ed51be29d148af2a9b6efb632c7545
David Gonzalo Ejea Carbonell <93073156+DavidGonzalo92@users.noreply.github.com>
2021-10-24 21:30:12 +0200

    Delete ProgramacionPython/TrabajoFinalDjango directory

--- ProgramacionPython/TrabajoFinalDjango/store/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(persona)
-admin.site.register(producto)
-admin.site.register(orden)
-admin.site.register(ordenproducto)
