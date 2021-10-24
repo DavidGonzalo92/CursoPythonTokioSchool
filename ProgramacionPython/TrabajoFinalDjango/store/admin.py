df170761eebf7e415d091dcdca6aea396f50697f
David Gonzalo Ejea Carbonell <93073156+DavidGonzalo92@users.noreply.github.com>
2021-10-24 21:28:58 +0200

    Add files via upload

---
+++ ProgramacionPython/TrabajoFinalDjango/store/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(persona)
+admin.site.register(producto)
+admin.site.register(orden)
+admin.site.register(ordenproducto)
